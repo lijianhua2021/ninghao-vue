80ab4cdff826741b49a0f0fee298a23e65018cfd
lijianhua2021 <lijianhua@sina.com>
2021-04-28 16:37:37 +0800

    单选按钮（Radio）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,19 +1,22 @@
 <template>
   <h3>{{ name }}</h3>
-  <label for="tag-1">
-    <input id="tag-1" type="checkbox" value="大明湖" v-model="tags" />
-    大明湖
+  <label for="subscription-annual">
+  <input 
+  id="subscription-annual" 
+  type="radio" 
+  value="annual" 
+  v-model="subscription" 
+  />一年
   </label>
-    <label for="tag-2">
-    <input id="tag-2" type="checkbox" value="黑虎泉" v-model="tags" />
-    黑虎泉
+  <label for="subscription-quarterly">
+  <input 
+  id="subscription-quarterly" 
+  type="radio" 
+  value="quarterly" 
+  v-model="subscription" 
+  />季度
   </label>
-    <label for="tag-3">
-    <input id="tag-3" type="checkbox" value="趵突泉" v-model="tags" />
-    趵突泉
-  </label><span>=>Tags: {{tags}}</span>
-
-
+  <span>=>Subscription: {{ subscription }}</span>
 </template>
 
 
@@ -22,7 +25,7 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      tags: []
+      subscription: 'annual'
     };
   },
 };
