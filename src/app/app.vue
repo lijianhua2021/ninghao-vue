f38755cc629173f05a5246436cdd7d86694f1d6e
lijianhua2021 <lijianhua@sina.com>
2021-04-28 10:06:23 +0800

    v-if：按条件显示内容

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,42 +1,22 @@
 <template>
-    <h3>{{ name }}</h3>
-    <div v-for="(post,index) in postlist " :key="index">
-    {{ index + 1 }} {{ post.content}}- <small>{{post.author}}</small>
-    </div>
+  <h3>{{ name }}</h3>
+  <div v-if="visible">隐藏的内容！</div>
+  <button @click="visible = !visible">{{ visible ? '隐藏' : '显示' }}</button>
 </template>
 
 
 <script>
-
 export default {
-    data() {
-        return {
-            name: 'NINGHAO',
-            postlist: [
-                {
-                id: 1,
-                content:'故人西辞黄鹤楼，烟花三月下扬州',
-                author: '李白',
-                },
-                {
-                id: 2,
-                content:'好雨知时节，当春乃发生',
-                author: '杜甫',
-
-                },
-                {
-                id: 3,
-                content:'浔阳江头夜送客，枫叶荻花秋瑟瑟',
-                author: '白居易',
-                }
-            ]
-        };
-    },
-
+  data() {
+    return {
+      name: '宁皓网',
+      visible: false,
+    };
+  },
 };
 </script>
 <style scoped>
 button {
-    margin: 4px;
+  margin: 4px;
 }
 </style>
