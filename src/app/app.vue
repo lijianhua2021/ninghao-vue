0bf4777ee2c998e340a244ec08c49f90bcb9679d
lijianhua2021 <lijianhua@sina.com>
2021-04-28 15:21:35 +0800

    在 Vue 组件里导入样式表文件

--- src/app/app.vue
+++ src/app/app.vue
@@ -2,10 +2,13 @@
   <h3>{{ name }}</h3>
   <div class="menu">
     <div 
-    :class="['menu-item',{ active: currentItem === index }]"
+    :class="[menu-item,{active: currentItem === index}]"
     @click="currentItem = index"
-    v-for="(item, index) in menuItems" :key="index">
-    {{ item }}</div>
+    v-for="(item,index) in menuItems" 
+    :key="index"
+    >
+    {{ item }}
+    </div>
   </div>
 </template>
 
@@ -15,18 +18,12 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      menuItems: ['首页','热门','发布'],
+      menuItems:['首页','热门','发布'],
       currentItem: 0
     };
   },
 };
 </script>
-<style scoped>
-.menu {
-  display: flex;
-  gap: 16px;
-}
-.active {
-  color: #6435c9;
-}
+<style>
+@import './styles/app.css';
 </style>
