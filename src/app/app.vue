8f6fde03bc1c352e3bf58e4436fc3f2b6d3acbfb
lijianhua2021 <lijianhua@sina.com>
2021-04-29 16:30:21 +0800

    定义与使用 Vue 组件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,27 +1,21 @@
 <template>
   <h3>{{ name }}</h3>
-  <div class="menu">
-    <div 
-    :class="[menu-item,{active: currentItem === index}]"
-    @click="currentItem = index"
-    v-for="(item,index) in menuItems" 
-    :key="index"
-    >
-    {{ item }}
-    </div>
-  </div>
+  <AppButton></AppButton>
+  <AppButton></AppButton>
+  <AppButton></AppButton>
+  <app-button></app-button>
 </template>
 
 
 <script>
+import AppButton from './components/app-button.vue';
 export default {
   data() {
     return {
       name: '宁皓网',
-      menuItems:['首页','热门','发布'],
-      currentItem: 0
     };
   },
+  components: {AppButton},
 };
 </script>
 <style>
