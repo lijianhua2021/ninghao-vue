c060de38e7168bd65130d11d187e8c291cdf739b
lijianhua2021 <lijianhua@sina.com>
2021-04-29 12:34:52 +0800

    v-model 的修饰符：.trim

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
-  <input type="text" v-model.number="content" @change="onChangeContent" />
+  <input type="text" v-model.trim="content" @change="onChangeContent" />
   <span>=> Content: {{ content }}</span>
 </template>
 
@@ -10,18 +10,14 @@ export default {
   data() {
     return {
       name: '宁皓网',
-      content: 0,
+      content: '',
     };
-  },
-
-  created(){
-    console.log('content 数据的类型(组件创建)：', typeof this.content);
   },
 
 
   methods: {
     onChangeContent(){
-      console.log('content 数据的类型 (元素值变化后): ', typeof this.content);
+      console.log(this.content, this.content.length);
     },
     },
 
